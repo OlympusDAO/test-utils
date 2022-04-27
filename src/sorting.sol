99e7775a3d4aa312513d6c92cb3eb1ba7250d2ef
PARABRAHMAN0 <noreply@ignore.com>
2022-04-28 00:03:45 +0200

    feat: new types

--- src/sorting.sol
+++ src/sorting.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.10;
 /**
  * @title Quicksort library in Solidity
  * @author Subhod I (https://gist.github.com/subhodi/b3b86cc13ad2636420963e692a4d896f),
- *         fried adapted to 0.8.10 and added memory variant
+ *         fried adapted to 0.8.10 and added memory variant + sortPartner
  */
 library sorting {
     function sort(
@@ -35,5 +35,41 @@ library sorting {
         if (i < right) sort(arr, i, right);
 
         return arr;
+    }
+
+    function sortPartner(
+        uint256[] memory arr,
+        uint256[] memory partner,
+        int256 left,
+        int256 right
+    ) internal returns (uint256[] memory, uint256[] memory) {
+        int256 i = left;
+        int256 j = right;
+
+        if (i == j) return (arr, partner);
+
+        uint256 pivot = arr[uint256(left + (right - left) / 2)];
+
+        while (i <= j) {
+            while (arr[uint256(i)] < pivot) i++;
+            while (pivot < arr[uint256(j)]) j--;
+            if (i <= j) {
+                (arr[uint256(i)], arr[uint256(j)]) = (
+                    arr[uint256(j)],
+                    arr[uint256(i)]
+                );
+                (partner[uint256(i)], partner[uint256(j)]) = (
+                    partner[uint256(j)],
+                    partner[uint256(i)]
+                );
+                i++;
+                j--;
+            }
+        }
+
+        if (left < j) sort(arr, left, j);
+        if (i < right) sort(arr, i, right);
+
+        return (arr, partner);
     }
 }
