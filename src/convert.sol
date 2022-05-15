a3351363d6e5211bd6e43f24f01732cf37e33e45
PARABRAHMAN0 <noreply@ignore.com>
2022-05-16 00:51:25 +0200

    feat: better convert naming due to fucking overloads not being unique enough even though this language is typed

--- src/convert.sol
+++ src/convert.sol
@@ -10,11 +10,11 @@ pragma solidity >=0.8.0;
 // organized in that order, ascending based on arg first then return
 library convert {
     /// uint224,uint256
-    function cu256(uint224 number) internal pure returns (uint256) {
+    function c224u256(uint224 number) internal pure returns (uint256) {
         return uint256(number);
     }
 
-    function cu256shl(uint224 number, uint256 shift)
+    function c224u256shl(uint224 number, uint256 shift)
         internal
         pure
         returns (uint256)
@@ -22,7 +22,7 @@ library convert {
         return uint256(number) << shift;
     }
 
-    function cu256shr(uint224 number, uint256 shift)
+    function c224u256shr(uint224 number, uint256 shift)
         internal
         pure
         returns (uint256)
@@ -94,11 +94,11 @@ library convert {
     }
 
     /// uint32,int32
-    function ci(uint32 number) internal pure returns (int32) {
+    function c32i(uint32 number) internal pure returns (int32) {
         return int32(number);
     }
 
-    function cishl(uint32 number, uint256 shift)
+    function c32ishl(uint32 number, uint256 shift)
         internal
         pure
         returns (int32)
@@ -106,7 +106,7 @@ library convert {
         return int32(number << uint32(shift));
     }
 
-    function cishr(uint32 number, uint256 shift)
+    function c32ishr(uint32 number, uint256 shift)
         internal
         pure
         returns (int32)
@@ -115,11 +115,11 @@ library convert {
     }
 
     /// uint224,int224
-    function ci(uint224 number) internal pure returns (int224) {
+    function c224i(uint224 number) internal pure returns (int224) {
         return int224(number);
     }
 
-    function cishl(uint224 number, uint256 shift)
+    function c224ishl(uint224 number, uint256 shift)
         internal
         pure
         returns (int224)
@@ -127,7 +127,7 @@ library convert {
         return int224(number << uint224(shift));
     }
 
-    function cishr(uint224 number, uint256 shift)
+    function c224ishr(uint224 number, uint256 shift)
         internal
         pure
         returns (int224)
